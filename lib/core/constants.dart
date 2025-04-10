5f6610e86f537bed06f9b053da3ca39e909b35f6
FTccr <furkantuccar@gmail.com>
2025-04-10 18:56:17 +0300

    Added project colors to constants file.

---
+++ lib/core/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class projectColor {
+  static Color PeriwinkleBlue = Color(0xff8e97fd);
+  static Color Black = Color(0xff0000);
+  static Color GreatFalls = Color(0xffA1A4B2);
+  static Color CloudBreak = Color(0xffF6F1FB);
+  static Color BleanchedAlmond = Color(0xffFFECCC);
+  static Color Intellectual = Color(0xff3F414E);
+  static Color CreamyApricot = Color(0xffFFE7BF);
+  static Color KantorBlue = Color(0xff03174D);
+  static Color ColdMorning = Color(0xffE5E5E5);
+}
